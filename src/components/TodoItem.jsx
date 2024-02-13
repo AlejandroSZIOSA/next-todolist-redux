b5ea89a11b2e59844d602a07fd30028fa34eb244
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-13 09:50:49 +0100

    some changes

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,29 +2,6 @@ import React from "react";
 //External variabels
 let btnDoneColor;
 
-const styles = {
-  CONTAINER: {
-    backgroundColor: "#E4E4E4",
-    display: "flex",
-    flexDirection: "row",
-    width: "600px",
-    border: "2px solid black",
-    borderRadius: "5px",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  BUTTONS_CONTAINER: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  TITLE_STYLE: {
-    fontSize: "x-large",
-    paddingLeft: "10px",
-    fontFamily: "cursive",
-  },
-};
-
 export default function TodoItem(props) {
   //Destructuring Item object :)
   const title = props.itemObj.title;
@@ -39,10 +16,10 @@ export default function TodoItem(props) {
   }
 
   return (
-    <div style={styles.CONTAINER}>
-      <div style={styles.TITLE_STYLE}>{title}</div>
-      <div style={styles.BUTTONS_CONTAINER}>
-        <div style={{ padding: "6px" }}>
+    <div>
+      <div>{title}</div>
+      <div>
+        <div>
           <button
             onClick={() => props.onClickDoneUndoneFn(id, done)} //Using arrow functions to pass params
             style={{
@@ -52,7 +29,7 @@ export default function TodoItem(props) {
             {done ? "Done" : "unDone"}
           </button>
         </div>
-        <div style={{ padding: "6px" }}>
+        <div>
           <button
             onClick={() => props.onClickRemoveItemFn(id)} //Using arrow functions to pass params :)
             style={{ backgroundColor: "red" }}
